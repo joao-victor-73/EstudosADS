a32ec2fabdccb3c6998a8195d77ef7ef1bd3d4e1
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-03-07 23:47:10 -0300

    questão 2 concluida

--- Desenvolvimento p Dispositivos Moveis/Exercicios Dart/sobre Funcoes/Questao2.dart
+++ Desenvolvimento p Dispositivos Moveis/Exercicios Dart/sobre Funcoes/Questao2.dart
@@ -18,10 +18,23 @@ String verificarPontuacao(List resultados) {
   return pontuacao.toString();
 }
 
-
+String subindoRank(var pont) {
+  // pont == pontuacao
+  if (pont > 60) {
+    return "Parabéns, você SUBIU DE PATENTE!";
+  } else if (pont >= 21 && pont <= 59) {
+    return "Você PERMANECE na mesma PATENTE!";
+  } else if (pont < 21) {
+    return "Você CAIU de PATENTE!";
+  }else {
+    return "Algo está errado!";
+  }
+}
 
 main() {
   List<String> listaResultados = [];
+
+  print("LEMBRANDO: V - VITORIA / E - EMPATE / D - DERROTA");
 
   for (int i = 10; i >= 1; i--) {
     stdout.write("Digite o resultado da $iº partida: ");
@@ -30,5 +43,9 @@ main() {
     listaResultados.add(resultado);
   }
 
-  print(verificarPontuacao(listaResultados));
+  String pontuacoesFinais = verificarPontuacao(listaResultados);
+  int pontuacoesFinaisConvertido = int.parse(pontuacoesFinais);
+
+  print("Jogador! Você tem uma pontuação de ${verificarPontuacao(listaResultados)} pontos.");
+  print("E com base nesses pontos, você... ${subindoRank(pontuacoesFinaisConvertido)}");
 }
