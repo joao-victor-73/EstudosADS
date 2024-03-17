a2b0c828696dffd0bb37565b5e832bda6b6e8913
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-03-17 09:04:39 -0300

    Create main.py

---
+++ Back-End Framework/Exercícios/Atividade Avaliativa 01/main.py
@@ -0,0 +1,12 @@
+print("=" * 60)
+print(f" <=========== {'SOFTWARE DE CADASTRO DE PRODUTOS'} ===========>")
+print("     1 - Cadastro de produto")
+print("     2 - Alterar produto")
+print("     3 - Listar e contar produto")
+print("     4 - Deletar produto")
+print("     5 - Sair do programa")
+print("=" * 60)
+
+
+# Tela de CADASTRO DE PRODUTOS
+print("Qual o tipo de produto vai cadastra?")
