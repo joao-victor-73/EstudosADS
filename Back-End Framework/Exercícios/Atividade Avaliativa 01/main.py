ddeaaf88c2e2efb407b0ad42c920fa5c1514c337
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-03-17 11:10:10 -0300

    tentando criar a 2º opcao

--- Back-End Framework/Exercícios/Atividade Avaliativa 01/main.py
+++ Back-End Framework/Exercícios/Atividade Avaliativa 01/main.py
@@ -1,82 +1,111 @@
+import cadastro as c
+
 
 def escolha_tipo_produto():
 
     print("=" * 60)
-    print(f" <=========== {'SOFTWARE DE CADASTRO DE PRODUTOS'} ===========>")
+    print(f"        <=========== {'SOFTWARE DE PRODUTOS'} ===========>")
     print("     1 - Alimentos")
     print("     2 - Bebidas")
     print("     3 - Produtos de Limpeza")
     print("     4 - Higiene")
     print("     5 - Outros")
-    print("     0 - Sair")
     print("=" * 60)
 
-    op_produtos = int(input("Qual a opção para cadastro? "))
+    op_produtos = int(input("Qual a opção desejada? "))
 
-    if op_produtos != 0:
-        return op_produtos
-    else:
-        return op_produtos
+    return op_produtos
 
 
 def cadastrando(tipo):
-        if tipo == 1:  # ALimento
-            prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
-            marca = input("Qual o nome da marca do produto?  ").capitalize()
 
-            produtos_alimentos[prod] = marca
+    if tipo == 1:  # ALimento
+        prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
 
-        elif tipo == 2:  # Bebidas
-            prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
-            marca = input("Qual o nome da marca do produto?  ").capitalize()
+        produtos_alimentos.append(prod)
 
-            produtos_bebidas[prod] = marca
+    elif tipo == 2:  # Bebidas
+        prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
 
-        elif tipo == 3:  # Produto de Limpeza
-            prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
-            marca = input("Qual o nome da marca do produto?  ").capitalize()
+        produtos_bebidas.append(prod)
 
-            produtos_limpeza[prod] = marca
+    elif tipo == 3:  # Produto de Limpeza
+        prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
 
-        elif tipo == 4:  # Higiene
-            prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
-            marca = input("Qual o nome da marca do produto?  ").capitalize()
+        produtos_limpeza.append(prod)
 
-            produtos_higiene[prod] = marca
+    elif tipo == 4:  # Higiene
+        prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
 
-        elif tipo == 5:  # Outros
-            prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
-            marca = input("Qual o nome da marca do produto?  ").capitalize()
+        produtos_higiene.append(prod)
 
-            produtos_outros[prod] = marca
+    elif tipo == 5:  # Outros
+        prod = input("Qual o nome do produto a ser armazenado:  ").capitalize()
+
+        produtos_outros.append(prod)
+
+
+def atualizacao(tipo):
+    if tipo == 1:  # ALimento
+        for p in produtos_alimentos:
+            print("Os produtos encontrados na Aba de alimentos foi: ")
+            print(f"{p} -", end=" ")
+
+
+def validacao():
+    print("=" * 60)
+    print(">> Caso queira adicionar mais produtos, digite S")
+    print(">> Caso queira sair do cadastramento de produtos, digite N.")
+    sair = input(">> [S] ou [N] ").upper().split()[0]
+    print("=" * 60)
+
+    return sair
+
 
 
 # Declarações de variáveis
-produtos_alimentos = {}
-produtos_bebidas = {}
-produtos_limpeza = {}
-produtos_higiene = {}
-produtos_outros = {}
+produtos_alimentos = []
+produtos_bebidas = []
+produtos_limpeza = []
+produtos_higiene = []
+produtos_outros = []
 
-print("=" * 60)
-print(f" <=========== {'SOFTWARE DE CADASTRO DE PRODUTOS'} ===========>")
-print("     1 - Cadastro de produto")
-print("     2 - Alterar produto")
-print("     3 - Listar e contar produto")
-print("     4 - Deletar produto")
-print("     5 - Sair do programa")
-print("=" * 60)
+def mensagem_inical():
+    print("=" * 60)
+    print(f" <=========== {'SOFTWARE DE CADASTRO DE PRODUTOS'} ===========>")
+    print("     1 - Cadastro de produto")
+    print("     2 - Alterar produto")
+    print("     3 - Listar e contar produto")
+    print("     4 - Deletar produto")
+    print("     5 - Sair do programa")
+    print("=" * 60)
 
-opcao = int(input("Informe a opção desejada: "))
+    op = int(input("Informe a opção desejada: "))
 
+    return op
+
+opcao = int(mensagem_inical())
+
+# Opçao de CADASTRAMENTO
 if opcao == 1:
     while True:
         cadastrando(escolha_tipo_produto())
 
         # If para parar o Laço infinito.
-        if escolha_tipo_produto() == 0:
+        if validacao() == 'N':
             break
-        else: 
+        else:
             continue
+    mensagem_inical()
 
-print(produtos_alimentos)
+# OPção para ALTERAR PRODUTO:
+elif opcao == 2:
+    while True:
+        atualizacao(escolha_tipo_produto())
+
+        # If para parar o Laço infinito.
+        if validacao() == 'N':
+            break
+        else:
+            continue
+    mensagem_inical()
