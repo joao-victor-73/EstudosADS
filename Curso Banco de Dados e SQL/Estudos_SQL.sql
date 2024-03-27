39776ab5f1405598deddab89801eb5034091d41a
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-03-27 07:52:41 -0300

    Estudos SQL

---
+++ Curso Banco de Dados e SQL/Estudos_SQL.sql
@@ -0,0 +1,291 @@
+/* OBS: Para o MySQL o comentario é feito através do # */
+
+/* 
+Para o primeiro momento, vamos criar uma base de dados com a seguinte tabela:
+
+clientes
+
+NOME - CARACTER(30)
+CPF - NUMERICO(11)
+EMAIL - CARACTER(30)
+TELEFONE - CARACTER(30)
+ENDERECO - CARACTER(100)
+SEXO - CARACTER(1)
+
+
+E para criar um banco de dados usamos o comando:
+CREATE DATABASE <nome do banco de dados>; */
+
+CREATE DATABASE clientes;
+
+/*
+O ; <ponto e virgula> é um ...
+*/
+
+/* Para visualizar os banco de dados já criados e novos: */
+SHOW DATABASES;
+
+/*Após criar um banco de dados, vamos começar a utiliza-lo*/
+USE clientes;
+
+/* Após dar um 'use' no BD, podemos criar nossas tabelas, editar, excluir e fazer outras coisas nas tabelas */
+CREATE TABLE clientes(
+    nome VARCHAR(30),
+    sexo CHAR(1),
+    email VARCHAR(30),
+    cpf INT(11),
+    telefone VARCHAR(30),
+    endereco VARCHAR(100)
+);
+
+/* Vamos verificar a criação e a estrutura da tabela criada */
+SHOW TABLES;
+DESC clientes;
+
+/*
++----------+--------------+------+-----+---------+-------+
+| Field    | Type         | Null | Key | Default | Extra |
++----------+--------------+------+-----+---------+-------+
+| nome     | varchar(30)  | YES  |     | NULL    |       |
+| sexo     | char(1)      | YES  |     | NULL    |       |
+| email    | varchar(30)  | YES  |     | NULL    |       |
+| cpf      | int          | YES  |     | NULL    |       |
+| telefone | varchar(30)  | YES  |     | NULL    |       |
+| endereco | varchar(100) | YES  |     | NULL    |       |
++----------+--------------+------+-----+---------+-------+    
+*/
+
+
+/*==========================================================================================*/
+/*                                      TIPOS DE DADOS                                      */
+/*==========================================================================================*/
+
+/* 
+- Todos os bancos de dados possuem tipos que devem ser atribuidos aos dados de uma tabela;
+
+    - Para caracteres literais, temos >CHAR< e >VARCHAR<;
+    - Para números temos: >INT< e >FLOAT<;
+    - Para objetos, como fotos e documentos, temos: >BLOB<;
+    - Para textos extensos temos: >TEXT<;
+
+    - Para dados do tipo de horários e datas temos: >DATA< / >TIME< / e >DATETIME<;
+    -Para tipos booleanos (true or false) temos: >BOOLEAN< / ou / >BOOL<;
+
+    (Para mais informações, há anotações em um caderno para mais aprofundamento!)
+
+- É interessante saber onde utilizar e em qual tipo de dado utilizar, pois assim pode deixar
+o banco de dados mais rápido, fácil e dinâmico.
+*/
+
+
+/*==========================================================================================*/
+/*                                      INSERÇÃO DE DADOS                                   */
+/*==========================================================================================*/
+
+/* 
+A maneira mais básica de inserir valores em uma tabela é usando a instrução INSERT INTO;
+Tem 3 maneiras comuns de se inserir dados dentro da tabela, são elas:  
+*/
+
+                                /* FORMA 01 - OMITINDO AS COLUNAS */
+
+INSERT INTO clientes VALUES('JOAO','M','JOAO@GMAIL.COM',988638273,'22923110','MAIA LACERDA - ESTACIO - RIO DE JANEIRO - RJ');
+
+INSERT INTO clientes VALUES('CELIA','F','CELIA@GMAIL.COM',541521456,'25078869','RIACHUELO - CENTRO - RIO DE JANEIRO - RJ');
+
+INSERT INTO clientes VALUES('JORGE','M',NULL,885755896,'58748895','OSCAR CURY - BOM RETIRO - PATOS DE MINAS - MG');
+
+                                /* FORMA 02 - COLOCANDO AS COLUNAS */
+/* Você especifica o nome da tabela seguido pelos nomes das colunas nas quais deseja inserir valores e, em seguida, os valores a serem inseridos. */
+
+INSERT INTO clientes(NOME,SEXO,ENDERECO,TELEFONE,CPF) VALUES('LILIAN','F','SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ','947785696',887774856);
+
+                                /* FORMA 03 - INSERT COMPACTO - SOMENTE MYSQL */
+
+INSERT INTO clientes VALUES('ANA','F','ANA@GLOBO.COM',85548962,'548556985','PRES ANTONIO CARLOS - CENTRO - SAO PAULO - SP'),
+                          ('CARLA','F','CARLA@TERATI.COM.BR',7745828,'66587458','SAMUEL SILVA - CENTRO - BELO HORIZONTE - MG');
+
+/* Vai dar erro esses de baixo.*/
+INSERT INTO clientes(NOME,SEXO,ENDERECO,TELEFONE,CPF) VALUES('CLARA','F','SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ','883665843',99999999999);
+
+INSERT INTO clientes(NOME,SEXO,ENDERECO,TELEFONE,CPF) VALUES('CLARA','F','SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ','883665843',22222222222);
+
+                                /* INSERT INTO de mútliplas Linhas */
+
+INSERT INTO nome_da_tabela (coluna1, coluna2) VALUES 
+    (valor1, valor2), 
+    (valor3, valor4), 
+    (valor5, valor6);
+
+INSERT INTO clientes (nome, sexo, endereco) VALUES
+('Francisca', 'F', 'Rua Freire Silva - n45'),
+('Livia', 'F', 'Rua Freire Silva - n65');
+
+
+/*==========================================================================================*/
+/*                                                 SELECT                                   */
+/*==========================================================================================*/
+
+/*
+A cláusula SELECT é uma das instruções mais fundamentais no SQL (Structured Query Language) e 
+é usada para recuperar dados de um banco de dados. Com a instrução SELECT, você pode especificar 
+quais colunas deseja recuperar e de quais tabelas ou fontes de dados deseja recuperar essas colunas.
+
+Se você quiser selecionar todas as colunas de uma tabela, pode usar o caractere curinga *: */
+SELECT * FROM clientes;
+
+/*                                              RESULTADO DA CONSULTA
++-----------+------+---------------------+-----------+-----------+-----------------------------------------------+
+| nome      | sexo | email               | cpf       | telefone  | endereco                                      |
++-----------+------+---------------------+-----------+-----------+-----------------------------------------------+
+| JOAO      | M    | JOAO@GMAIL.COM      | 988638273 | 22923110  | MAIA LACERDA - ESTACIO - RIO DE JANEIRO - RJ  |
+| CELIA     | F    | CELIA@GMAIL.COM     | 541521456 | 25078869  | RIACHUELO - CENTRO - RIO DE JANEIRO - RJ      |
+| JORGE     | M    | NULL                | 885755896 | 58748895  | OSCAR CURY - BOM RETIRO - PATOS DE MINAS - MG |
+| LILIAN    | F    | NULL                | 887774856 | 947785696 | SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ |
+| ANA       | F    | ANA@GLOBO.COM       |  85548962 | 548556985 | PRES ANTONIO CARLOS - CENTRO - SAO PAULO - SP |
+| CARLA     | F    | CARLA@TERATI.COM.BR |   7745828 | 66587458  | SAMUEL SILVA - CENTRO - BELO HORIZONTE - MG   |
+| Francisca | F    | NULL                |      NULL | NULL      | Rua Freire Silva - n45                        |
+| Livia     | F    | NULL                |      NULL | NULL      | Rua Freire Silva - n65                        |
++-----------+------+---------------------+-----------+-----------+-----------------------------------------------+
+*/
+
+/* Em casos em que queremos específicar as colunas da tabela, nos chamamos isso de PROJEÇÃO;
+a projeção se refere à parte de uma consulta SELECT que especifica quais colunas devem ser incluídas 
+no conjunto de resultados. Em outras palavras, é a seleção das colunas que você deseja ver na saída da consulta.
+
+
+Selecionando colunas específicas de uma tabela:*/
+SELECT nome, email, sexo FROM clientes;
+
+/*          RESULTADO DA CONSULTA:
++-----------+---------------------+------+
+| nome      | email               | sexo |
++-----------+---------------------+------+
+| JOAO      | JOAO@GMAIL.COM      | M    |
+| CELIA     | CELIA@GMAIL.COM     | F    |
+| JORGE     | NULL                | M    |
+| LILIAN    | NULL                | F    |
+| ANA       | ANA@GLOBO.COM       | F    |
+| CARLA     | CARLA@TERATI.COM.BR | F    |
+| Francisca | NULL                | F    |
+| Livia     | NULL                | F    |
++-----------+---------------------+------+
+*/
+
+
+/* O QUE É O ALIAS EM MYSQL?
+    -> um alias é um nome alternativo ou apelido temporário atribuído a uma coluna, tabela ou 
+        expressão em uma consulta. Isso é útil para tornar o resultado da consulta mais legível, 
+        fornecer nomes mais descritivos ou simplificar referências a colunas ou tabelas com nomes 
+        longos ou complexos.
+    
+    -> Existem dois tipos principais de alias no SQL:
+
+        1. ALIAS DE COLUNA
+            Um alias de coluna é usado para renomear uma coluna no resultado da consulta. 
+            Isso é útil quando você deseja dar um nome mais significativo a uma coluna ou 
+            quando está usando funções ou expressões em uma coluna e deseja atribuir um nome 
+            mais descritivo ao resultado;
+
+        2. ALIAS DE TABELAS
+            Um alias de tabela é usado para fornecer um nome abreviado ou apelido para uma 
+            tabela na consulta, facilitando a referência a ela, especialmente em consultas que 
+            envolvem várias tabelas. 
+    
+    -> Um alias de tabela é usado para fornecer um nome abreviado ou apelido para uma tabela na consulta, 
+        facilitando a referência a ela, especialmente em consultas que envolvem várias tabelas.
+
+    EXEMPLO: */ 
+SELECT nome, email, sexo AS genero FROM clientes;
+
+/* RESULTADO DA CONSULTA:
++-----------+---------------------+--------+
+| nome      | email               | genero |
++-----------+---------------------+--------+
+| JOAO      | JOAO@GMAIL.COM      | M      |
+| CELIA     | CELIA@GMAIL.COM     | F      |
+| JORGE     | NULL                | M      |
+| LILIAN    | NULL                | F      |
+| ANA       | ANA@GLOBO.COM       | F      |
+| CARLA     | CARLA@TERATI.COM.BR | F      |
+| Francisca | NULL                | F      |
+| Livia     | NULL                | F      |
++-----------+---------------------+--------+
+*/
+
+/*==========================================================================================
+                                    CLÁUSULA WHERE (filtro)                                  
+  ==========================================================================================
+  -> A cláusula WHERE é usada em consultas SQL para filtrar linhas com base em uma condição 
+  especificada. Essa condição é avaliada para cada linha na tabela, e apenas as linhas que 
+  atendem à condição são incluídas no resultado da consulta.
+  
+  Sintaxe básica:
+  SELECT <coluna> 
+  FROM <tabela>
+  WHERE <condição>;
+  */
+
+SELECT nome, telefone FROM clientes
+WHERE sexo = 'M';
+
+/* RESULTADO DA CONSULTA COM FILTRO
++-------+----------+
+| nome  | telefone |
++-------+----------+
+| JOAO  | 22923110 |
+| JORGE | 58748895 |
++-------+----------+
+*/
+
+SELECT nome, email, telefone FROM clientes
+WHERE telefone > 25078869;
+
+/* RESULTADO DA CONSULTA COM FILTRO
++--------+---------------------+-----------+
+| nome   | email               | telefone  |
++--------+---------------------+-----------+
+| JORGE  | NULL                | 58748895  |
+| LILIAN | NULL                | 947785696 |
+| ANA    | ANA@GLOBO.COM       | 548556985 |
+| CARLA  | CARLA@TERATI.COM.BR | 66587458  |
++--------+---------------------+-----------+
+*/
+
+/*                              >>>>> UTILIZANDO O LIKE  <<<<<
+    -> A cláusula LIKE é usada em consultas SQL para realizar correspondências de padrões
+    em valores de texto. Ela é comumente usada em conjunto com a cláusula WHERE para filtrar 
+    as linhas de uma tabela com base em padrões de texto específicos. 
+    -> O LIKE permite que você busque por padrões, como uma parte específica de uma palavra, 
+    uma palavra que comece ou termine com determinados caracteres, entre outros.
+
+    SINTAXE BÁSICA:
+    SELECT colunas
+    FROM tabela
+    WHERE coluna LIKE padrão;
+*/
+
+SELECT nome, email, endereco FROM clientes
+WHERE endereco LIKE '%RJ';
+
+/* RESULTADO DA CONSULTA COM LIKE
++--------+-----------------+-----------------------------------------------+
+| nome   | email           | endereco                                      |
++--------+-----------------+-----------------------------------------------+
+| JOAO   | JOAO@GMAIL.COM  | MAIA LACERDA - ESTACIO - RIO DE JANEIRO - RJ  |
+| CELIA  | CELIA@GMAIL.COM | RIACHUELO - CENTRO - RIO DE JANEIRO - RJ      |
+| LILIAN | NULL            | SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ |
++--------+-----------------+-----------------------------------------------+
+*/
+
+SELECT nome, email FROM clientes
+WHERE email LIKE '%gmail%';
+
+/* RESULTADO DA CONSULTA COM LIKE de padrão específico em qualquer lugar
++-------+-----------------+
+| nome  | email           |
++-------+-----------------+
+| JOAO  | JOAO@GMAIL.COM  |
+| CELIA | CELIA@GMAIL.COM |
++-------+-----------------+
+*/
