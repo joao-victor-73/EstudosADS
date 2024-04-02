81e1c130fb685bba01d2d4b579c11d6b78ac5691
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-04-02 07:14:35 -0300

    Update alter.sql

--- Curso Banco de Dados e SQL/Anotacoes/alter.sql
+++ Curso Banco de Dados e SQL/Anotacoes/alter.sql
@@ -15,6 +15,29 @@ tabela já existente.
         ADD nome_da_coluna tipo_de_dado;
         ---------------------------------------------------------------------------------
 
+        A posição das colunas em uma tabela é geralmente determinada pelo banco de dados 
+        e não é considerada uma propriedade fixa. 
+        
+        No entanto, você pode definir a posição relativa da nova coluna em relação às 
+        colunas existentes usando a cláusula AFTER ou FIRST, dependendo do SGBD que 
+        está utilizando.
+
+        USANDO AFTER:
+            Você pode usar a cláusula AFTER para especificar que a nova coluna deve 
+            ser adicionada após uma coluna específica existente.
+
+            ALTER TABLE nome_da_tabela
+            ADD nome_da_coluna tipo_de_dado AFTER nome_da_coluna_anterior;
+
+
+        USANDO FIRST:
+            Você pode usar a cláusula FIRST para especificar que a nova coluna deve 
+            ser adicionada como a primeira coluna na tabela.
+
+            ALTER TABLE nome_da_tabela
+            ADD nome_da_coluna tipo_de_dado FIRST;
+        ---------------------------------------------------------------------------------
+
 
     >>> MODIFICAR UMA COLUNA EXISTENTE:
         Você pode alterar a definição de uma coluna existente, como seu tipo de dado ou 
@@ -22,6 +45,24 @@ tabela já existente.
 
         ALTER TABLE nome_da_tabela
         MODIFY nome_da_coluna novo_tipo_de_dado;
+        ---------------------------------------------------------------------------------
+
+        Também dá para utilizar o CHANGE no lugar de MODIFY.
+
+        O CHANGE é usado para renomear uma coluna e/ou modificar seu tipo de dado ou 
+        suas restrições em uma tabela existente. 
+        
+        Em alguns sistemas de gerenciamento de banco de dados (SGBDs), como MySQL, o 
+        CHANGE pode ser usado para alcançar o mesmo resultado que o MODIFY, além de 
+        permitir a alteração do nome da coluna simultaneamente.
+
+        ALTER TABLE nome_da_tabela
+        CHANGE nome_antigo nome_novo tipo_de_dado;
+        ---------------------------------------------------------------------------------
+
+        A dica é:
+            -> Quando quiser trabalhar com mudanças no nome de uma coluna, utilizar o CHANGE;
+            -> Quando quiser traballhar para mudar o tipo do dado, utilizar o MODIFY.
         ---------------------------------------------------------------------------------
 
 
@@ -61,5 +102,5 @@ modificar a estrutura de uma tabela existente em um banco de dados.
 O ALTER TABLE oferece flexibilidade para fazer ajustes na estrutura do banco de 
 dados conforme necessário, sem precisar recriar a tabela inteira.
 
-
 */
+
