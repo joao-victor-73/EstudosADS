86bf8935a29aa63984acbb5645a265ded6c7b86f
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-04-02 10:31:29 -0300

    mini projeto de cadastramento
    Só foi feito o trabalho de HTML, CSS e também o framework de Bootstrap para facilitar a parte visual

---
+++ projetos/projetinho/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def principal():
+    return render_template('index.html')
+
+
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
+
+@app.route('/lista')
+def lista():
+    return render_template('lista.html')
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
