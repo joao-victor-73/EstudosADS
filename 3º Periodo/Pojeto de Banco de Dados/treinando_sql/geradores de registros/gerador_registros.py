0f97311e0c48198037944dcb28e67ce45a561b5c
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-06-02 15:38:33 -0300

    Terminando de adicionar os registros

--- 3º Periodo/Pojeto de Banco de Dados/treinando_sql/geradores de registros/gerador_registros.py
+++ 3º Periodo/Pojeto de Banco de Dados/treinando_sql/geradores de registros/gerador_registros.py
@@ -23,17 +23,64 @@ mydb = mysql.connector.connect(
 
 # Criando uma lista de MARCAS DE PRODUTOS para ser gerado aleatoriamente
 marcas_tecnologia = [
-
+    "Apple",
+    "Dell",
+    "HP",
+    "Lenovo",
+    "Asus",
+    "Acer",
+    "Motorola",
+    "Multilaser",
+    "Samsung",
+    "Huawei",
+    "Xiaomi",
+    "Sony",
+    "LG",
+    "Kingston",
+    "Seagate",
+    "Corsair",
+    "Nvidia"
 ]
 
 # Criando uma lista de NOMES DE PRODUTOS para ser gerado aleatoriamente
 produto_tecnologia = [
-
+    "Notebook",
+    "Tablet",
+    "Monitor",
+    "Teclado",
+    "Mouse",
+    "Webcam",
+    "Impressora",
+    "Scanner",
+    "HD",
+    "SSD",
+    "Headphone",
+    "HeadSet",
+    "Fones de Ouvido",
+    "Capas para celular",
+    "Capas para tablet",
+    "Capas para Notebook",
+    "Suportes para Carro",
+    "Suportes para Moto",
+    "Cabos e Adaptadores",
+    "Projetores",
+    "Console PS4",
+    "Console PS5",
+    "Console Xbox One",
+    "Console Xbox X/S",
+    "Controle/Joystick",
+    "Caixas de Som Bluetooth",
+    "Microfones",
+    "Tripés"
 ]
 
 # Criando uma lista de NOMES DE EMPRESA para ser gerado aleatoriamente
 nome_fornecedores = [
-
+    "Amazon",
+    "Newegg",
+    "Walmart",
+    "Kabum",
+    "Micro Center"
 ]
 
 
@@ -58,15 +105,20 @@ for _ in range(10):  # Gere 10 registros fictícios
 
 for _ in range(10):
     preco_faker = round(fake.pyfloat(
-        min_value=10, max_value=100, right_digits=2), 2) 
+        min_value=10, max_value=100, right_digits=2), 2)
     # gerador de valores de preco entre 10 a 100
-    
-    em_estoque_faker = fake.random_int(min=1, max=20) # gerador de produtos em estoque entre 0 a 20
+
+    # gerador de produtos em estoque entre 0 a 20
+    em_estoque_faker = fake.random_int(min=1, max=20)
 
     nomeProduto = fake.random_element(produto_tecnologia)
     marca = fake.random_element(marcas_tecnologia)
     preco = preco_faker
     em_estoque = em_estoque_faker
+
+    SQL = "INSERT INTO produtos (nomeProduto, marca, preco, em_estoque) VALUES (%s, %s, %s, %s)"
+    val = (nomeProduto, marca, preco, em_estoque)
+    cursor.execute(SQL, val)
 
 
 # Gerar e inserir dados fictícios para a tabela 'fornecedor'
@@ -78,6 +130,10 @@ for _ in range(10):
     email = fake.email()
     telefone = fake.phone_number()
     CEP = fake.postcode()
+
+    SQL = "INSERT INTO fornecedor (nomeFornecedor, rua, bairro, cidade, email, telefone, CEP) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = (nomeFornecedor, rua, bairro, cidade, email, telefone, CEP)
+    cursor.execute(SQL, val)
 
 
 mydb.commit()
